a0423d3b22a96c3e3f3b1b531a43baea5561a248
Cap PT <phatasdasd0198@gmail.com>
2024-04-03 20:45:08 +0700

    #5

--- src/views/Example/MyComponent.js
+++ src/views/Example/MyComponent.js
@@ -2,9 +2,38 @@ import React from 'react';
 
 
 class MyComponent extends React.Component {
+
+    state = {
+        name: 'Cap',
+        channel: 'PT'
+    }
+
+    handleOnChangeName = (event) => {
+        this.setState({
+            name: event.target.value
+        })
+    }
+
     render() {
+        let name = 'CapPT';
         return (
-            <div>Hello my component CapPT</div>
+            <>
+                <div>
+                    <input value={this.state.name} type='text'
+                        onChange={(event) => this.handleOnChangeName(event)}
+                    >
+
+                    </input>
+
+                    {console.log('My name is : ', name)}
+                    Hello my component, My name is {this.state.name}
+
+                </div>
+
+                <div>
+                    My youtube channel : this.state.channel
+                </div>
+            </>
         )
     }
 }
