886d245548d813c10462d04463aac8120c39db66
Cap PT <phatasdasd0198@gmail.com>
2024-04-05 15:54:32 +0700

    #21.2

--- src/views/Todos/ListTodo.js
+++ src/views/Todos/ListTodo.js
@@ -11,7 +11,17 @@ class ListTodo extends React.Component {
             { id: 'todo2', title: 'Making video' },
             { id: 'todo3', title: 'Doing homework' },
 
-        ]
+        ],
+
+        editTodo: {}
+    }
+
+    handleDeleteTodo = (todo) => {
+        this.setState({
+            ListTodo: this.state.ListTodo.filter(item => item.id !== todo.id)
+        });
+
+        toast.success('Delete success!')
     }
 
     addNewTodo = (todo) => {
@@ -22,8 +32,50 @@ class ListTodo extends React.Component {
 
     }
 
+    handleEditTodo = (todo) => {
+
+        let { editTodo, ListTodo } = this.state;
+        let isEmptyObj = Object.keys(editTodo).length === 0
+        if (isEmptyObj === false && editTodo.id === todo.id) {
+
+            let listTodosCopy = [...ListTodo];
+            let objIndex = listTodosCopy.findIndex(item => item.id === todo.id);
+
+
+            listTodosCopy[objIndex].title = editTodo.title;
+            this.setState({
+                ListTodo: listTodosCopy,
+                editTodo: {}
+
+            })
+            toast.success('Edit successfully')
+            return;
+        }
+
+        this.setState({
+            editTodo: todo
+        })
+
+        return;
+
+    }
+
+    handleOnchangeEditTodo = (event) => {
+        let editTodoCOpy = { ...this.state.editTodo };
+        editTodoCOpy.title = event.target.value;
+        this.setState({
+            editTodo: editTodoCOpy
+        })
+    }
+
+
+
     render() {
-        let { ListTodo } = this.state;
+
+
+        let { ListTodo, editTodo } = this.state;
+
+        let isEmptyObj = Object.keys(editTodo).length === 0
         return (
             <div className="list-todo-container">
                 <AddTodo addNewTodo={this.addNewTodo} />
@@ -32,9 +84,36 @@ class ListTodo extends React.Component {
                     {ListTodo && ListTodo.length > 0 && ListTodo.map((item, index) => {
                         return (
                             <div className="todo-child" key={item.id}>
-                                <span>{index + 1} - {item.title}</span>
-                                <button className="edit"> Edit</button>
-                                <button className="delete"> Delete</button>
+
+                                {isEmptyObj === true ?
+                                    <span>{index + 1} - {item.title}</span>
+
+                                    :
+                                    <>
+                                        {editTodo.id === item.id ?
+                                            <span>
+                                                {index + 1} - <input value={editTodo.title}
+                                                    onChange={(event) => this.handleOnchangeEditTodo(event)}
+                                                ></input>
+                                            </span>
+                                            :
+                                            <span>
+                                                {index + 1} - {item.title}
+                                            </span>
+                                        }
+                                    </>
+                                }
+                                <button className="edit"
+                                    onClick={() => this.handleEditTodo(item)}
+                                >
+                                    {isEmptyObj === false && editTodo.id === item.id ?
+                                        'Save' : 'Edit'
+                                    }
+                                </button>
+                                <button className="delete"
+                                    onClick={() => this.handleDeleteTodo(item)}
+
+                                > Delete</button>
                             </div>
                         )
                     })}
